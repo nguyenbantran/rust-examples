7569441e59e6f9ccc944c22368fbc71a09b8d3fc
Ban Tran <bantn@outlook.com>
2025-12-31 21:11:22 +0700

    feat(async): basic future example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use crate::async1::feature::future_main;
 use crate::async1::tasks::task_main;
 
 mod async1;
 
 #[tokio::main]
 async fn main() {
-    let _ = task_main().await;
+    if false {
+        // Example 01: execute async tasks
+        task_main().await;
+    }
+
+    // Example 02: some basic future aspect
+    future_main().await
 }
